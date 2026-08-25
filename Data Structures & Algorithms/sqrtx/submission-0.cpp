181471973793c4c640346e307ed54926935ffc28
Hong Won Gi <55803332+keyone957@users.noreply.github.com>
2026-08-25 15:56:39 +0900

    Add: sqrtx - submission-0

---
+++ Data Structures & Algorithms/sqrtx/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+           if (x < 2) return x;
+
+        long long left = 1;
+        long long right = x / 2;
+        long long answer = 0;
+
+        while (left <= right)
+        {
+            long long mid = (left + right) / 2;
+            long long square = mid * mid;
+
+            if (square == x)
+            {
+                return mid;
+            }
+            else if (square < x)
+            {
+                answer = mid;
+                left = mid + 1;
+            }
+            else
+            {
+                right = mid - 1;
+            }
+        }
+
+        return answer;
+    }
+};
